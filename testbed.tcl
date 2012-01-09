75de5df3e7108020a590256a9a658291256dbf19
mxmanghi@apache.org <mxmanghi@apache.org>
2012-01-09 23:14:31 +0000

    version of sitemap manages that loads menus and build a tree. Consistency yet to be checked: highly unstable

---
+++ testbed.tcl
@@ -0,0 +1,11 @@
+lappend auto_path .
+package require rwconf
+package require rwlogger
+package require rwlink
+package require rwmenu
+package require rwsitemap
+package require XMLData
+package require rivetweb
+
+::rivetweb::init . website XMLData
+
