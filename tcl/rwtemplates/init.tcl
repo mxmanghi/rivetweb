19137470e781b9009b59f47fb44b137bf74033ae
mxmanghi@apache.org <mxmanghi@apache.org>
2012-01-18 22:29:05 +0000

    nuova forma di init.tcl salvata nella directory templates per la creazione di nuovi siti

--- tcl/rwtemplates/init.tcl
+++ tcl/rwtemplates/init.tcl
@@ -1,17 +1,16 @@
 # $Id: init.tcl 2098 2011-12-15 09:41:47Z massimo.manghi $
 
-set rweb_root /home/manghi/Projects/rivetweb-sf
+set rweb_root /home/manghi/rivetweb/
 
 lappend auto_path  $rweb_root
 package require rivetweb
 
-::rivetweb::init $rweb_root [file dirname [info script]] XMLData
-
-if {[info exists ::rivetweb::apache_running]} {
-    apache_log_error info "starting rivetweb for website at $::rivetweb::site_base"
-}
+::rivetweb::setup $rweb_root [file normalize [file dirname [info script]]]
 
 # rivetweb initialization 
 
-source [file join $::rivetweb::scripts rivetweb_init.tcl]
 source [file join $::rivetweb::site_base site_defs.tcl]
+source [file join $::rivetweb::scripts rivetweb_init.tcl]
+::rivetweb::init XMLData XMLMenu
+
+
