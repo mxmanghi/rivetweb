8de54b731f6ec04d863bf7c980b03f1d945c6d36
massimo.manghi@unipr.it <massimo.manghi@unipr.it>
2012-03-19 08:45:26 +0000

    wrong handling of local_pages corrected

--- tcl/hooks/programlisting.tcl
+++ tcl/hooks/programlisting.tcl
@@ -25,8 +25,7 @@ proc programlisting {element_text attribute_list} {
 
     if {[info exists attributes(src)]} {
 
-        set code_file $attributes(src)
-        set code_file   [::rivetweb::searchPath $code_file [list $::rivetweb::static_pages \
+        set code_file [::rivetweb::searchPath $attributes(src) [list $::rivetweb::static_pages \
                                                                  $::rivetweb::site_base    \
                                                                  $::rivetweb::rivetweb_root]]
 
@@ -67,54 +66,3 @@ proc programlisting {element_text attribute_list} {
     return $d
 }
 
-#proc programlisting {xmlDoc child} {
-#
-#    if {[$child hasAttribute src]} {
-#
-#        set code_file   [$child getAttribute src]
-#
-#        set code_file   [::rivetweb::searchPath $code_file [list $::rivetweb::static_pages \
-#                                                                 $::rivetweb::site_base    \
-#                                                                 $::rivetweb::rivetweb_root]]
-#
-#        if {[string length $code_file]} {
-#
-#            set code_fp     [open $code_file r]
-#            set code_text   [read $code_fp]
-##debug      apache_log_error debug "text in $code_file:\n $code_text"
-##debug      apache_log_error debug [escape_sgml_chars $code_text]
-#            close $code_fp    
-#
-#        } else {
-#
-#            set code_text ""
-#
-#        }
-#
-#        $xmlDoc createTextNode $code_text newTextNode
-#        set newPreNode [$xmlDoc createElement pre]
-#        $newPreNode setAttribute class programlisting
-#        $newPreNode appendChild $newTextNode
-#        [$child parentNode] replaceChild $newPreNode $child
-##debug  apache_log_error debug "replacing $child with $newPreNode ([[$newPreNode parentNode] asText])"
-#
-#    } else {
-#        set newPreNode [$xmlDoc createElement pre]
-#        $newPreNode setAttribute class programlisting
-#
-#        foreach plChild [$child childNodes] {
-#            set nodeText [$plChild asXML -indent 2]
-#
-#            regsub -all {(&lt;)} $nodeText "<" unescaped_text
-#            regsub -all {(&gt;)} $unescaped_text ">" unescaped_text
-#
-##debug      apache_log_error err "appending:\n $unescaped_text"
-#            $xmlDoc createTextNode $unescaped_text newTextNode
-#            $newPreNode appendChild $newTextNode
-#        }
-#        [$child parentNode] replaceChild $newPreNode $child
-#    }
-#    $child delete
-#
-#    return $xmlDoc
-#}
