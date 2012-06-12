bc358a616f6d4e6ff6e750f93297b7b1ea0d12cd
mxmanghi@apache.org <mxmanghi@apache.org>
2012-06-12 14:45:14 +0000

    add .gitignore to skip versioning of init.tcl. Base init.tcl is in tcl/rwtemplates

--- tcl/rwtemplates/init.tcl
+++ tcl/rwtemplates/init.tcl
@@ -1,8 +1,17 @@
 # $Id: init.tcl 2098 2011-12-15 09:41:47Z massimo.manghi $
 
-set rweb_root /home/manghi/rivetweb/
+# this stuff must go in rivetweb's configuration, I don't see any other
+# clear way to make it site specific
+#
+# set rweb_root /home/manghi/Projects/rivetweb/
+#
 
-lappend auto_path  $rweb_root
+set rweb_root [file normalize [file join ..]]
+
+lappend auto_path $rweb_root
+
+puts stderr "auto_path: $auto_path"
+
 package require rivetweb
 
 ::rivetweb::setup $rweb_root [file normalize [file dirname [info script]]]
@@ -11,6 +20,6 @@ package require rivetweb
 
 source [file join $::rivetweb::site_base site_defs.tcl]
 source [file join $::rivetweb::scripts rivetweb_init.tcl]
-::rivetweb::init XMLData XMLMenu
+::rivetweb::init $::rivetweb::datasource $::rivetweb::menusource 
 
 
