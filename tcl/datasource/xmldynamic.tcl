f686b062348ecd7586d17fb806d725be9c874c8a
mxmanghi@apache.org <mxmanghi@apache.org>
2012-11-08 11:32:52 +0000

    nuova datasource dinamica per script specifici di un sito

---
+++ tcl/datasource/xmldynamic.tcl
@@ -0,0 +1,20 @@
+#
+# -- xmldynamic.tcl
+#
+#
+#
+#
+
+package require rwconf
+package require rwlogger
+package require rwsitemap
+package require rwpmodel
+
+namespace eval ::XMLScripted {
+    variable sitemap
+    variable script_path tcl
+
+    proc init {xml
+
+
+}
