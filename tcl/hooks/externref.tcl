30d143596276acd393384bd8ccb6b1c883200824
mxmanghi@apache.org <mxmanghi@apache.org>
2013-01-02 22:59:02 +0000

    obsoled code for externref hook removed

--- tcl/hooks/externref.tcl
+++ tcl/hooks/externref.tcl
@@ -18,23 +18,3 @@ proc externref {element_text attribute_list} {
     return $d
 }
 
-#proc externref {domDoc child} {
-#    
-#    if {[$child hasAttribute href]} {
-#        set anchor_text [$child asText]
-#
-#        $domDoc createTextNode $anchor_text new_anchor_txt
-#        set newAnchorElement [$domDoc createElement a]
-#        foreach refattr [$child attributes] {
-#            set attvalue [$child getAttribute $refattr]
-#            $newAnchorElement setAttribute $refattr $attvalue
-#        }
-#        $newAnchorElement setAttribute target _blank 
-#
-#        $newAnchorElement appendChild $new_anchor_txt
-#        [$child parentNode] replaceChild $newAnchorElement $child
-#        $child delete     
-#    }
-#
-#    return $domDoc
-#}
