9873f17d530ca7259b1721c5a82716216a186a28
mxmanghi@apache.org <mxmanghi@apache.org>
2013-06-07 15:49:09 +0000

    nasty bug in localref fixed

--- tcl/hooks/localref.tcl
+++ tcl/hooks/localref.tcl
@@ -14,17 +14,17 @@ proc localref {datasource tag element_text attribute_list} {
 
     if {[info exists attributes(alias)] && [$datasource get_alias $attributes(alias) aliasdef]} { 
 
-        set file_path [join [list $::rivetweb::local_pages $aliasdef] "/"]
+        set file_path "/$::rivetweb::local_pages/$aliasdef"
         set attributes(href) $file_path
         unset attributes(alias)
 
     } elseif {[info exists attributes(href)]} {   
 
-        set attributes(href) [join [list $::rivetweb::local_pages $attributes(href)] "/"]
+        set attributes(href) "/$::rivetweb::local_pages/$attributes(href)"
 
     } elseif {[info exists attributes(src)]} {
 
-        set attributes(src) [join [list $::rivetweb::local_pages $attributes(src)] "/"]
+        set attributes(src) "/$::rivetweb::local_pages/$attributes(src)"
 
     }
     dict set d text $element_text
