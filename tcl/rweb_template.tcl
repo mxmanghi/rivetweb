5213b3886c1fb669af0ffbd5d77b47cdcf7d38fd
mxmanghi@apache.org <mxmanghi@apache.org>
2017-09-09 17:07:36 +0000

    moving towards a template manager class

---
+++ tcl/rweb_template.tcl
@@ -0,0 +1,101 @@
+# -- rweb_template.tcl
+#
+#
+#
+#
+
+package require Itcl
+
+namespace eval ::rivetweb {
+
+    variable menuclass              RWMenu
+
+    ::itcl::class RWTemplate {
+        public common templates_db     [dict create]
+
+        # defaults taken from rwbase/rwtemplate.tcl
+
+        private variable rwtemplate	    xhtml10.rvt
+        private variable rwcss	        xhtml10.css
+        private variable menu_html      {li ""}
+        private variable title_html     {h2 ""}
+        private variable it_cont_html   {ul ""}
+        private variable item_html      {li ""}
+        private variable link_class     navitem
+        private variable menuclass      RWMenu
+
+        public method init {descriptor} 
+        public method getprop {prop}
+        public method serialize {}
+        public method register_formatter {formatter}
+
+        public proc load_templates {templates_root}
+        public proc template {template_key {prop ""}}
+    }
+
+    ::itcl::body RWTemplate::getprop {prop} {
+
+        if {$prop == "css"} { return $rwcss }
+        if {$prop == "template"} { return $rwtemplate }
+
+        if {[info exists $prop]} {
+            return [set $prop]
+        } else {
+            return ""
+        }
+    }
+
+    ::itcl::body RWTemplate::serialize {} {
+        return [dict create css             $rwcss \
+                            template        $rwtemplate \
+                            menu_html       $menu_html  \
+                            title_html      $title_html \
+                            it_cont_html    $it_cont_html \
+                            item_html       $item_html \
+                            link_class      $link_class \
+                            menuclass       $menuclass]
+    }
+
+    ::itcl::body RWTemplate::init {descriptor_file} {
+        source $descriptor_file
+    }
+
+    ::itcl::body RWTemplate::register_formatter {formatter} {
+        namespace eval [namespace current]::formatters $formatter 
+    }
+
+    ::itcl::body RWTemplate::load_templates {templates_dir} {
+
+        foreach template [glob -directory $templates_dir *] {
+            if {[file isdirectory $template]} {
+
+                set template_key [file tail $template]
+
+                #puts "searching for [file join $template rwtemplate.tcl]"
+
+                set base_descriptor [file join $template rwtemplate.tcl]
+                if {[file exists $base_descriptor]} {
+                    set template_o [::rivetweb::RWTemplate [namespace current]::${template_key}]
+                    $template_o init $base_descriptor
+
+                    dict set templates_db $template_key $template_o
+                }
+
+                set formatters [file join $template formatters.tcl]
+            }
+        }
+
+    }
+
+    ::itcl::body RWTemplate::template {template_key {prop ""}} {
+        if {![dict exists $templates_db $template_key]} { return "" }
+        set template_o [dict get $templates_db $template_key]
+        if {$prop == ""} {
+            return [$template_o as_dict]
+        } else {
+            return [$template_o getprop $prop]
+        }
+    }
+}
+
+package provide RWTemplate 1.0
