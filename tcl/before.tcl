66263971000781b653a097b8bc7edea929158141
manghi <manghi>
2018-08-21 14:45:48 +0000

    removing old now out of place comment

--- tcl/before.tcl
+++ tcl/before.tcl
@@ -16,19 +16,11 @@ namespace eval ::rivetweb {
     # let's load the environment into array ::request::env
 
     ::rivet::load_env env
-    ::rivet::apache_log_error debug "running tcl/before.tcl"
+    ::rivet::apache_log_error debug "running rivetweb general tcl/before.tcl"
 
-# let's assign the controlling variables with the corresponding parameters 
-# definitions.
-#
-# static   -> enabling generation of 'static' (i.e. .html) pages
-# homepage -> the home is treated in a slightly different way, so we have
-#             to signal it with this flag
-#
-# this assignements are meaningful only when appropriate mod_rewrite rules
-# are set up in the configuration 
-
-#   set ::rivetweb::static_links [::rivet::var_qs exists static]
+# determining if the 'rewrite_par' argument is in the query
+# list of arguments and in case set the rewrite_links flag
+# and the 'rewrite_code' free form code
 
     set rewrite_par [$::rivetweb::url_composer get_rewrite_par]
     set ::rivetweb::rewrite_links [::rivet::var_qs exists $rewrite_par]
@@ -142,7 +134,7 @@ namespace eval ::rivetweb {
         if {[catch {set ::rivetweb::current_page [$::rivetweb::datasource fetch_page $::rivetweb::page_key ::rivetweb::page_key]} e einfo]} {
             set ::rivetweb::current_page [::rivetweb simple_page fetch_page_error [::rivetweb make_error_page $e $einfo]]
         }
-    } 
+    }
     $::rivetweb::logger log info "processing request for '$::rivetweb::page_key'"
 
 #
